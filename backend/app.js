807919ca7a2b43b4d95dc1aaab4d97bbe56ecdc2
amaan mulla <amaanmulla07@gmail.com>
2025-01-11 21:39:25 +0530

    added origin url in app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://dp-frontend-rosy.vercel.app'],
   credentials: true
 }));
 
