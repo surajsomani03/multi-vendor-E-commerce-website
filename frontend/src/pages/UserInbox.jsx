c47610e08b0c034652b90eb88cb420a24e9518d3
amaan mulla <amaanmulla07@gmail.com>
2025-01-12 14:31:13 +0530

    socketID variable added

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,8 +9,8 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-// const ENDPOINT = "https://socket-ecommerce-tu68.onrender.com/";
-// const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
+const ENDPOINT = "https://socket-ecommerce-tu68.onrender.com/";
+const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
   const { user } = useSelector((state) => state.user);
